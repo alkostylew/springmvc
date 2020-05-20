9270eb018ac8ea693dff4680d72c01f7f20fd0b2
alkostylew <aleks.kostylew@gmail.com>
2020-05-20 11:38:38 -0300

    Using DB Authentication

--- src/main/java/org/sid/springmvc/security/SecurityConfig.java
+++ src/main/java/org/sid/springmvc/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.sid.springmvc.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,9 +8,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import sun.security.provider.MD5;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -17,9 +24,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         System.out.println("*****************");
         System.out.println(passwordEncoder.encode("1234"));
         System.out.println("*****************");
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username as principal, password as credentials, active from users where username=?")
+                .authoritiesByUsernameQuery("select username as principal, role as role from users_roles where username=?")
+                .passwordEncoder(passwordEncoder)
+                .rolePrefix("ROLE_");
+        /*
         auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("1234")).roles("USER");
         auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1234")).roles("USER");
         auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN");
+         */
     }
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
